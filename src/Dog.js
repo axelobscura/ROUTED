5bce6ecad1c93422801393f6e2b7d7ec4ccc85aa
Axel <axosar@gmail.com>
2019-08-13 18:13:17 -0500

    Updating the Dog Route
    Creating the dog route

---
+++ src/Dog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Dog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>
+        DOG COMPONENT
+      </div>
+    );
+  }
+}
+ 
+export default Dog;
