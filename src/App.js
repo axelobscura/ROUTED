48fef9b23f6263879f42380455154ab6460cc547
Axel <axosar@gmail.com>
2019-08-13 18:54:33 -0500

    Adding Link Router
    Mapping to links from tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import About from './About';
 import Dog from './Dog';
 import Contact from './Contact';
@@ -10,6 +10,14 @@ const Hater = () => <h1>I ABSOLUTELY LOVE DOGS</h1>;
 function App() {
   return (
     <div className="App">
+      <div className="nav">
+        <div className="App-nav">
+          <Link to="/">About</Link>
+          <Link to="/dog">Dog</Link>
+          <Link to="/hater">Hater</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
       <Switch>
         <Route exact path='/' component={About} />
         <Route exact path='/dog' component={Dog} />
