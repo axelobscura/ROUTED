75c2cae3aaf126665e49acb99c1613116c2d4cb3
Axel <axosar@gmail.com>
2019-08-18 18:53:29 -0500

    tests
    test

--- src/Dog.js
+++ src/Dog.js
@@ -12,11 +12,11 @@ class Dog extends Component {
     console.log('DOG DID UNMOUNT!!!')
   }
   render() {
-    console.log('DOG RENDERD'); 
+    console.log('DOG RENDERD');
     return (
       <div>
         <h1>THE DOG</h1>
-        <h2>{this.props.name}</h2>
+        <h2>{this.props.name} - some data</h2>
         <img src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&auto=format&fit=crop&w=360&q=80" alt=""/>
       </div>
     );
